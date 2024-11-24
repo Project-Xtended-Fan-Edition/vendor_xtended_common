9f701126f19e799c0a90cbaa959cb4a269d8537e
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-11-24 20:17:55 +0900

    vendor: Clean-ups
    * Remove dup to vendor/lineage for future dev
    
    Change-Id: I6edb1e81bd33275a3fcebdfc90f9a81a622d3b5e

--- config/common/apps.mk
+++ config/common/apps.mk
@@ -12,17 +12,12 @@ PRODUCT_PACKAGES += \
     libbluetooth_jni
 endif
 
-# Camera
+# Aperture
 ifneq ($(PRODUCT_NO_CAMERA),true)
-ifneq ($(TARGET_INCLUDE_APERTURE),false)
-PRODUCT_PACKAGES += \
-    Aperture
-
 PRODUCT_PACKAGES += \
     PixelConfigOverlayAperture \
     PixelLauncherCustomOverlayAperture \
     SystemUICustomOverlayAperture
-endif #TARGET_INCLUDE_APERTURE
 endif #PRODUCT_NO_CAMERA
 
 # Game Space
@@ -33,19 +28,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SystemUIFlagFlipper
 
-# SystemUI plugins
-PRODUCT_PACKAGES += \
-    QuickAccessWallet
-
 # TouchGestures
 PRODUCT_PACKAGES += \
     TouchGestures
-
-# Updater
-ifneq ($(TARGET_INCLUDE_UPDATER),false)
-PRODUCT_PACKAGES += \
-    Updater
-endif #TARGET_INCLUDE_UPDATER
 
 # Face Unlock
 ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
