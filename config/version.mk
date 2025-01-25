EverestOS-AOSP/vendor_everest_common

vendor: Build GAPPS conditionally

config/version.mk
@@ -10,7 +10,7 @@ CUSTOM_BUILD_DATE := $(shell date +%Y%m%d-%H%M)
 CUSTOM_BUILD_DATE_UTC := $(shell date '+%Y-%m-%d %H:%M UTC %s')
 CUSTOM_BUILD_VERSION := $(BUILD_ID)
 
-CUSTOM_VERSION := $(CUSTOM_BRAND)-$(EVEREST_VERSION)-$(EVEREST_BUILDTYPE)_$(LINEAGE_BUILD)-$(BUILD_ID)-$(CUSTOM_BUILD_DATE)
+CUSTOM_VERSION := $(CUSTOM_BRAND)-$(EVEREST_VERSION)-$(EVEREST_BUILDTYPE)_$(LINEAGE_BUILD)-$(EVEREST_EDITION)-$(CUSTOM_BUILD_DATE)
 
 PRODUCT_BRAND := $(CUSTOM_BRAND)
 LINEAGE_BUILD_DATE := $(CUSTOM_BUILD_DATE)
@@ -27,4 +27,5 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.modversion=$(LINEAGE_VERSION) \
     ro.everest.buildtype=$(LINEAGE_BUILDTYPE) \
     ro.everest.version=$(EVEREST_VERSION) \
-    ro.everest.codename=$(EVEREST_CODENAME)
+    ro.everest.codename=$(EVEREST_CODENAME) \
+    ro.everest.edition=$(EVEREST_EDITION)

Project-Xtended-Fan-Edition/vendor_xtended_common

config/version.mk
@@ -10,7 +10,7 @@ CUSTOM_BUILD_DATE := $(shell date +%Y%m%d-%H%M)
 CUSTOM_BUILD_DATE_UTC := $(shell date '+%Y-%m-%d %H:%M UTC %s')
 CUSTOM_BUILD_VERSION := $(BUILD_ID)
 
-CUSTOM_VERSION := $(CUSTOM_BRAND)-$(XTENDED_VERSION)-$(XTENDED_BUILDTYPE)_$(LINEAGE_BUILD)-$(BUILD_ID)-$(CUSTOM_BUILD_DATE)
+CUSTOM_VERSION := $(CUSTOM_BRAND)-$(XTENDED_VERSION)-$(XTENDED_BUILDTYPE)_$(LINEAGE_BUILD)-$(XTENDED_EDITION)-$(CUSTOM_BUILD_DATE)
 
 PRODUCT_BRAND := $(CUSTOM_BRAND)
 LINEAGE_BUILD_DATE := $(CUSTOM_BUILD_DATE)
@@ -27,4 +27,5 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.modversion=$(LINEAGE_VERSION) \
     ro.xtended.buildtype=$(LINEAGE_BUILDTYPE) \
     ro.xtended.version=$(XTENDED_VERSION) \
-    ro.xtended.codename=$(XTENDED_CODENAME)
+    ro.xtended.codename=$(XTENDED_CODENAME) \
+    ro.xtended.edition=$(XTENDED_EDITION)